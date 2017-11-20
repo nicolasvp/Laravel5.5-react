43140d7c33b6f0dd2d45ae6f3856449c29ced62a
Nicolás Vera <nicolasverapalominos@gmail.com>
2017-11-20 11:53:33 -0300

    comentarios en español para la paginacion

--- resources/assets/js/components/Pagination.js
+++ resources/assets/js/components/Pagination.js
@@ -12,7 +12,7 @@ class PaginationListComponent extends Component {
     }
  
     componentWillMount() {
-        // set page if items array isn't empty
+        // Setea las paginas si los items no están vacios
         if (this.props.items && this.props.items.length) {
             this.setPage(this.props.initialPage);
         }
@@ -29,7 +29,7 @@ class PaginationListComponent extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        // reset page if items array has changed
+        // Resetea las paginas si cambiaron los items
         if (this.props.items !== prevProps.items) {
             this.setPage(this.props.initialPage);
         }
@@ -43,36 +43,31 @@ class PaginationListComponent extends Component {
             return;
         }
  
-        // get new pager object for specified page
         pager = this.getPager(items.length, page);
  
-        // get new page of items from items array
         var pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
  
-        // update state
         this.setState({ pager: pager });
 
-        // call change page function in parent component
         this.props.onChangePage(pageOfItems,pager.currentPage);
     }
  
     getPager(totalItems, currentPage, pageSize) {
-        // default to first page
         currentPage = currentPage || 1;
  
-        // default page size is 10
+        // Cantidad de paginas por default = 2
         pageSize = pageSize || 2;
  
-        // calculate total pages
+        // Calcula el total de páginas
         var totalPages = Math.ceil(totalItems / pageSize);
  
         var startPage, endPage;
         if (totalPages <= 10) {
-            // less than 10 total pages so show all
+            // Si el total es menor que 10 entonces muestra todo
             startPage = 1;
             endPage = totalPages;
         } else {
-            // more than 10 total pages so calculate start and end pages
+            // Si es mayor a 10 entonces calcula el inicio y fin
             if (currentPage <= 6) {
                 startPage = 1;
                 endPage = 10;
@@ -85,14 +80,12 @@ class PaginationListComponent extends Component {
             }
         }
  
-        // calculate start and end item indexes
+        // Calcula el index de inicio y fin
         var startIndex = (currentPage - 1) * pageSize;
         var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
  
-        // create an array of pages to ng-repeat in the pager control
         var pages = _.range(startPage, endPage + 1);
  
-        // return object with all pager properties required by the view
         return {
             totalItems: totalItems,
             currentPage: currentPage,
@@ -110,7 +103,7 @@ class PaginationListComponent extends Component {
         var pager = this.state.pager;
  
         if (!pager.pages || pager.pages.length <= 1) {
-            // don't display pager if there is only 1 page
+            // No muestra la paginación si sólo hay una 1 página
             return null;
         }
  
